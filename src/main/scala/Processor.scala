03c81b58d8bccf0eab0dd377cda95c513dd9c633
Vishal-Mandan <83539666+Vishal-Mandan@users.noreply.github.com>
2021-06-23 20:53:57 +0530

    Update Processor.scala

--- src/main/scala/Processor.scala
+++ src/main/scala/Processor.scala
@@ -1,24 +1,30 @@
+import java.io.PrintWriter
 import java.nio.file.Paths
 
 import scala.io.Source
 
 object Processor extends App {
+
   def process(fileName: String) {
     val fileSource = Source.fromFile(Paths.get("src\\main\\scala\\" + fileName).toFile)
     val lines: Seq[String] = fileSource.getLines().toSeq
+    val writer = new PrintWriter("output.txt")
 
 
     val opString = lines.map {
       line =>
-        line.split(" ").map {
+        val transformedLine = line.split(" ").map {
           word =>
             val firstChar = word.charAt(0)
             if (firstChar.isLower) word.replace(firstChar, firstChar.toUpper) else firstChar
         }.mkString(" ")
+        writer.write(transformedLine)
     }
 
-    println(opString)
+    writer.close()
   }
+
+  //  val outputFile= new File(Paths.get("src\\main\\scala\\output.txt"))
 
   process("test1.txt")
 }
